0ffffaf15ab43e77227e61877664c184b9dff828
takashina8929 <sakura_nanana8929@docomo.ne.jp>
2020-04-27 16:31:06 +0900

    initial

---
+++ index.php
@@ -0,0 +1,66 @@
+<?php
+// エラーメッセージの初期化
+$emptyId = "";
+$emptyPass = "";
+$errorMessage = "";
+
+// できない
+// ログインボタンが押された場合
+// if (isset($_POST["login"])) {
+//     // 1. ユーザIDの入力チェック
+//     if (empty($_POST["id"])) { 
+//         $emptyId = 'IDが未入力です。';
+//     }
+//     if (empty($_POST["password"])) {
+//         $emptyPass = 'パスワードが未入力です。';
+//     }
+
+//     header("Location: success.php");
+//     exit();
+// }
+
+// セッション開始
+session_start();
+
+
+if (isset($_POST["login"])) {
+    $id = $_GET["id"];
+    $pass = $_GET["password"];
+    
+    echo $id;
+    echo $pass;
+
+    header("Location: ./success.php");
+}
+?>
+
+
+<!doctype html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <title>demo</title>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+
+<body>
+    <header>
+        <h1 class="headline">
+            <a>DEMO</a>
+        </h1>
+    </header>
+
+    <form method="POST" action="">
+        <div>
+        <p class="label">ID
+        <input type="text" name="id" id="id" class="input"></p>
+        <p class="label">PASS
+        <input type="password" name="password" class="input"></p>
+    </div>
+        <p><input type="submit" value="LOGIN" id="login" class="button"></p>
+    </form>
+</body>
+</head>
+
+</html>
---
+++ success.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <title>demo</title>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+
+<body>
+    <header>
+        <h1 class="headline">
+            <a>DEMO</a>
+        </h1>
+    </header>
+
+    <div>
+    success !
+    <br>
+    <?php $id + $pass ?>
+    </div>
+</body>
+</head>
+
+</html>
